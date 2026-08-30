07820d4873ff0034224d16977dfcce992181c229
asingizwe1 <asingizwe1louis1@gmail.com>
2026-08-30 22:12:03 +0300

    [STYLE] Add hero gradient headline, AOS animations, and grain-textured background

---
+++ src/components/home/PageIllustration.tsx
@@ -0,0 +1,8 @@
+export function PageIllustration() {
+    return (
+        <div
+            className="pointer-events-none absolute left-[30%] top-[-150px] -z-10 h-[500px] w-[700px] rounded-full bg-[#00FFAB]/[0.15] blur-[160px]"
+            aria-hidden="true"
+        />
+    );
+}
